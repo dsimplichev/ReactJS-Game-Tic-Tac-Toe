148ddb4b67d2b7264037542cceac204e82a93e63
dsimplichev <dsimplichev1948@gmail.com>
2024-01-23 12:47:46 +0000

    working on board wich player is active

--- src/App.jsx
+++ src/App.jsx
@@ -1,18 +1,26 @@
+import { useState } from "react"
+
 import Header from "./components/Header/Header"
 import Player from "./components/Player/Player"
 import GameBoard from "./components/GameBoard/GameBoard"
 
 function App() {
+  const [activePlayer, setActivePlayer] = useState('X');
+   
+  function handleSelectSquare() {
+    setActivePlayer((curActivePlayer) => curActivePlayer === 'X' ? 'O' : 'X'); 
+  }
+  
   return (
     <>
    <Header />
    <main>
      <div id='game-container'>
-      <ol id='players'>
-       <Player initialName='player 1' symbol='X'/>
-       <Player initialName='player 2' symbol='O'/>
+      <ol id='players' className="highlight-player">
+       <Player initialName='player 1' symbol='X' isActive={activePlayer === 'X'}/>
+       <Player initialName='player 2' symbol='O' isActive={activePlayer === 'O'}/>
       </ol>
-      <GameBoard />
+      <GameBoard onSelectSquare={handleSelectSquare} />
      </div>
    </main>
    </>
