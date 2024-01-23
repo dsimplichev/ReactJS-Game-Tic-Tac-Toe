fae37ad52cf02fb9c4ee7a24da45b027f34313fb
dsimplichev <dsimplichev1948@gmail.com>
2024-01-23 10:40:17 +0000

    add function change name

--- src/components/Player/Player.jsx
+++ src/components/Player/Player.jsx
@@ -1,24 +1,29 @@
 import { useState } from "react"
 
-export default function Player({name, symbol}) {
+export default function Player({initialName, symbol}) {
+    const [playerName, setPlayerName] = useState(initialName)
     const [isEditing, setIsEditing] = useState(false)
 
     function handleEditClick() {
-        setIsEditing(true)
+        setIsEditing((editing) => !editing);
     }
 
-    let playerName = <span className="player-name">{name}</span>;
+     function handleChange() {
+        setPlayerName();
+     }
+
+    let editablePlayerName = <span className="player-name">{playerName}</span>;
     let btnCaption = 'Edit'
     
     if(isEditing) {
-        playerName = <input className="input" type="text" required />
+        editablePlayerName = <input className="input" type="text" required value={playerName} onChange={handleChange}/>
         btnCaption='Save'
     }
 
     return(
         <li>
         <span>
-         {playerName}
+         {editablePlayerName}
         <span className="player-symbol">{symbol}</span>
         </span>
         <button onClick={handleEditClick}>{btnCaption}</button>
